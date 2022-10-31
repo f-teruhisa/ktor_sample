44980c69cb59a43ec36790581cd922315083b3a3
f-teruhisa <teru_fukumoto@outlook.jp>
2022-10-31 23:17:12 +0900

    Devide articles routing into ArticleController

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,15 +1,12 @@
 package com.example.plugins
 
-import com.example.dao.dao
 import com.example.controller.*
 import com.example.models.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.http.content.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.util.*
 
 fun Application.configureRouting() {
     routing {
@@ -25,48 +22,9 @@ fun Application.configureRouting() {
             val sampleUser = User(1, "John")
             call.respond(FreeMarkerContent("index.ftl", mapOf("user" to sampleUser)))
         }
-        
-        route("articles") {
-            get {
-                call.respond(FreeMarkerContent("articles/index.ftl", mapOf("articles" to dao.allArticles())))
-            }
-            get("new") {
-                call.respond(FreeMarkerContent("articles/new.ftl", model = null))
-            }
-            post {
-                val formParameters = call.receiveParameters()
-                val title = formParameters.getOrFail("title")
-                val body = formParameters.getOrFail("body")
-                val article = dao.addNewArticle(title, body)
-                call.respondRedirect("/articles/${article?.id}")
-            }
-            get("{id}") {
-                val id = call.parameters.getOrFail<Int>("id").toInt()
-                call.respond(FreeMarkerContent("articles/show.ftl", mapOf("article" to dao.article(id))))
-            }
-            get("{id}/edit") {
-                val id = call.parameters.getOrFail<Int>("id").toInt()
-                call.respond(FreeMarkerContent("articles/edit.ftl", mapOf("article" to dao.article(id))))
-            }
-            post("{id}") {
-                val id = call.parameters.getOrFail<Int>("id").toInt()
-                val formParameters = call.receiveParameters()
-                when (formParameters.getOrFail("_action")) {
-                    "update" -> {
-                        val title = formParameters.getOrFail("title")
-                        val body = formParameters.getOrFail("body")
-                        dao.editArticle(id, title, body)
-                        call.respondRedirect("/articles/$id")
-                    }
-                    "delete" -> {
-                        dao.deleteArticle(id)
-                        call.respondRedirect("/articles")
-                    }
-                }
-            }
-        }
     }
     routing {
+        articleController()
         userController()
     }
 }
